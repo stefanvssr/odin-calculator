9b95a4b05660f0c60c920ab71958133f7f4dd3e6
Stefan Visser <me@stefanvssr.dev>
2022-11-27 20:17:55 +0100

    Create calcDivide function

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ function calcMultiply(num1, num2) {
     return num1 * num2;
 }
 
+function calcDivide(num1, num2) {
+    return num1 / num2;
+}
+
 console.log(calcAdd(30, 123));
 console.log(calcSubtract(150, 22));
 console.log(calcMultiply(10, 11));
+console.log(calcDivide(10, 5));
