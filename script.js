edf06e14941f4966ce10915602fbdd2b70612d26
Stefan Visser <me@stefanvssr.dev>
2022-11-27 20:14:07 +0100

    Create calcSubtract function

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ function calcAdd(num1, num2) {
     return num1 + num2;
 }
 
+function calcSubtract(num1, num2) {
+    return num1 - num2;
+}
+
 console.log(calcAdd(30, 123));
+console.log(calcSubtract(150, 22));
