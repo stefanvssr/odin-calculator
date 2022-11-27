91ec50e9928189038b7002e7adc7306ee5a5df67
Stefan Visser <me@stefanvssr.dev>
2022-11-27 20:17:30 +0100

    Create calcMultiply function

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ function calcSubtract(num1, num2) {
     return num1 - num2;
 }
 
+function calcMultiply(num1, num2) {
+    return num1 * num2;
+}
+
 console.log(calcAdd(30, 123));
 console.log(calcSubtract(150, 22));
+console.log(calcMultiply(10, 11));
